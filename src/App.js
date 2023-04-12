f9986f777b3d1414a79104de024877e4861a633b
NaufalFiqri <naufal11bale@gmail.com>
2023-04-12 17:07:38 +0800

    init explore section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from "./components/Landing.jsx";
 import Highlights from "./components/Highlights.jsx";
 import Featured from "./components/Featured.jsx";
 import Discounted from "./components/Discounted.jsx";
+import Explore from "./components/Explore.jsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Highlights />
       <Featured />
       <Discounted />
+      <Explore />
     </div>
   );
 }
