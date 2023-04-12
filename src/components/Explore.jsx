b0bc08b2ef1104b08d657d104435d8c36b6d5c14
NaufalFiqri <naufal11bale@gmail.com>
2023-04-12 17:11:06 +0800

    finished explore section

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Explore = () => {
-    return (
-        Explore
-    );
-}
+  return (
+    <section id="explore">
+      <div className="container">
+        <div className="row row__column">
+          <h2>
+            Explore more <span className="purple">Books</span>
+          </h2>
+          <a href="/books">
+            <button className="btn">Explore books</button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Explore;
