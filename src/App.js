3f5802885533ab20b7543f4f444eb9e7e75f33a1
NaufalFiqri <naufal11bale@gmail.com>
2023-04-17 14:59:17 +0800

    added to cart button func added

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import Footer from "./components/Footer.jsx";
 import Books from "./pages/Books.jsx";
 import BookInfo from "./pages/BookInfo.jsx";
 import Cart from "./pages/Cart.jsx";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { books } from "./data";
 
 function App() {
   const [cart, setCart] = useState([]);
 
-  function addToCart() {
-    console.log('add to cart')
+  function addToCart(book) {
+    setCart([...cart, book])
   }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Route />
         <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
         <Route path="/cart" render={() => <Cart books={books} />} />
         <Footer />
       </div>
